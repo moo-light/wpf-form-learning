c4bedea4d17df2e701e57cc4a21244f1e65df611
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2023-04-28 14:53:59 +0700

    Added WPF GridSplitter

--- WPF_MessageBox/obj/Debug/net7.0-windows/WPF_MessageBox.AssemblyInfo.cs
+++ WPF_MessageBox/obj/Debug/net7.0-windows/WPF_MessageBox.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WPF_MessageBox")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WPF_MEssagebox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WPF_MessageBox")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WPF_MessageBox")]
+[assembly: System.Reflection.AssemblyProductAttribute("WPF_MEssagebox")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WPF_MEssagebox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
